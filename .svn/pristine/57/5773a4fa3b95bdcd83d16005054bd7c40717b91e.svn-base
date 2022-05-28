e4baf03055dd1bb0b8f4bfbd75fadeaec8076f3e
plumspark <plumspark@users.noreply.github.com>
2022-05-28 11:42:40 -0600

    project init
    published all files from local project to repo

---
+++ .svn/pristine/57/5773a4fa3b95bdcd83d16005054bd7c40717b91e.svn-base
@@ -0,0 +1,74 @@
+﻿namespace ica04
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.tbxOutput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(107, 35);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(171, 48);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // tbxOutput
+            // 
+            this.tbxOutput.Location = new System.Drawing.Point(12, 116);
+            this.tbxOutput.Multiline = true;
+            this.tbxOutput.Name = "tbxOutput";
+            this.tbxOutput.Size = new System.Drawing.Size(365, 247);
+            this.tbxOutput.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(389, 375);
+            this.Controls.Add(this.tbxOutput);
+            this.Controls.Add(this.btnOpen);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.TextBox tbxOutput;
+    }
+}
+
---
+++ ica04/Form1.cs
@@ -0,0 +1,78 @@
+﻿/* ICA04 - LINQ
+ * September 28 2021
+ * By Liam Carroll for CMPE2800 1211
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ica04
+{
+    public partial class Form1 : Form
+    {
+
+        List<string> sourcestrings = new List<string>(
+            new string[] { "Caballo", "Gato", "Perro", "Conejo", "Tortuga", "Cangrejo" });
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //select the strings where the sum is less than 600
+            var partA1 = from animals in sourcestrings where animals.Sum(ch => ch) < 600 select animals;
+            foreach (var animal in partA1) tbxOutput.Text += $"{animal}{Environment.NewLine}";
+            //same thing but put them into an anonymous type
+            var partA2 = from animals in sourcestrings where animals.Sum(ch => ch) < 600 select new { Str = animals, Sum = animals.Sum(ch => ch) };
+            foreach (var animal in partA2) tbxOutput.Text += $"{animal}{Environment.NewLine}";
+            //Order the anonymous types (see above) by the sum and select the anonymous types
+            var partA3 = from animals in sourcestrings where animals.Sum(ch => ch) < 600 select new { Str = animals, Sum = animals.Sum(ch => ch) } into newSet orderby newSet.Sum descending select newSet;
+            foreach (var animal in partA3) tbxOutput.Text += $"{animal}{Environment.NewLine}";
+
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            FileDialog dialog = new OpenFileDialog();
+
+            if(dialog.ShowDialog() == DialogResult.OK)
+            {
+                tbxOutput.Text = "";
+
+                var sr = new StreamReader(dialog.FileName);
+                var fileBuffer = sr.ReadToEnd();
+
+                var fileContents = from i in fileBuffer.Split(new char[] { '\r', '\n', '\t' }) where i != "" select i;
+
+                //Create a grouped collection where the key is the sum and the group is the items that share that sum
+                var fileGrouped = from i in fileContents group i by i.Sum(ch => ch);
+
+                //Create a new grouped collection sorted by key, select first group and return key
+                tbxOutput.Text += $"Lowest ASCII Sum : {(from i in fileGrouped orderby i.Key ascending select i).First().Key} {Environment.NewLine}";
+                //Select the items from the grouped collection sorted by key, order them and return the first item from that group
+                tbxOutput.Text += $"Lowest String : {(from j in (from i in fileGrouped orderby i.Key ascending select i).First() orderby j select j).First() }{Environment.NewLine}";
+                //Create a new grouped collection sorted by key, select last group and return key
+                tbxOutput.Text += $"Highest ASCII Sum : {(from i in fileGrouped orderby i.Key ascending select i).Last().Key} {Environment.NewLine}";
+                //select the items from the grouped collection sorted by key, order them and return the last item from that group / do the same thing but order that and do a string concat on the character selector
+                tbxOutput.Text += $"Lowest String : {(from j in (from i in fileGrouped orderby i.Key ascending select i).Last() orderby j select j).Last() }" +
+                    $"/{String.Concat(from ch in ((from j in (from i in fileGrouped orderby i.Key ascending select i).Last() orderby j select j).Last()) orderby ch select ch) } " +
+                    $"{Environment.NewLine}";
+                //Create a new grouped collection sorted by count of items in groups, return the last groups count, and then select its key
+                tbxOutput.Text += $"Biggest Collection Count : {(from i in fileGrouped orderby i.Count() select i).Last().Count()} " +
+                    $"- ASCII Sum : {(from i in fileGrouped orderby i.Count() select i).Last().Key}";
+
+            }
+        }
+
+        
+    }
+}
